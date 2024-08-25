44b5789a83d6c48d28b54b2e26d68f1bca1f00ce
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-08-25 20:02:40 +0300

    Add user resource access check to AuthGuard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,4 +1,9 @@
-﻿import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+﻿import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
@@ -26,10 +31,18 @@ export class AuthGuard implements CanActivate {
     if (!token) {
       throw new UnauthorizedException('Token is missing');
     }
+
     try {
-      request['user'] = await this.jwtService.verifyAsync(token, {
+      const user = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
+
+      const userIdFromUrl = this.extractUserIdFromUrl(request);
+      if (userIdFromUrl && Number(userIdFromUrl) !== user.sub) {
+        return false;
+      }
+
+      request['user'] = user;
     } catch (error) {
       if (error.name === 'TokenExpiredError') {
         throw new UnauthorizedException('Token expired');
@@ -42,5 +55,10 @@ export class AuthGuard implements CanActivate {
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
+  }
+
+  private extractUserIdFromUrl(request: Request): string | undefined {
+    const match = request.url.match(/^\/api\/users\/(\d+)/);
+    return match ? match[1] : undefined;
   }
 }
